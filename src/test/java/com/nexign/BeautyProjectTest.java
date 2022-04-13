d640452d87d1859d31dc53f4f79177fce865efb7
Alisa.Kadilenko <alisa.kadilenko@nexign.com>
2022-04-13 23:45:40 +0300

    New test

---
+++ src/test/java/com/nexign/BeautyProjectTest.java
@@ -0,0 +1,37 @@
+package com.nexign;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+
+public class BeautyProjectTest {
+
+    //Vars
+    String  url = "https://beauty-project.ru/calculator.html",
+            validExpDate = "https://beauty-project.ru/calculator.html",
+            isExpired = "https://beauty-project.ru/calculator.html";
+
+    @CsvSource(value = {
+            "Nars|0247BC",
+            "Dolce&Gabbana|9269DL",
+            "MAC|9269DL",
+    },
+            delimiter = '|'
+    )
+    @ParameterizedTest(name = "Проверка косметики по бренду {0} и батч-коду {1}")
+    void searchBeautyProject(String brand, String code) {
+        //Предусловия
+        Selenide.open(url);
+        //Шаги
+        $("#brand").$(byText(brand)).click();
+        $("#code").setValue(code);
+        $("button[type='submit']").click();
+        //Ожмдаемый результат
+        $$("#Result").find(Condition.text(validExpDate))
+                .shouldNotHave(Condition.text(isExpired));
+    }
+}
